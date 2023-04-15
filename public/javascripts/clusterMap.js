64bd89ce1465090fc9fae2aeda644c6dd38e3254
arnav <arnav@pop-os.localdomain>
2023-04-15 23:20:04 +0530

    changes in index.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-103.5917, 40.6699],
+    center: [77.102,28.7040,],
     zoom: 3
 });
 
